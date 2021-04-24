e735391b59c4ce7fd03345d28fee20899685244b
Long Ly Thien <long.lythien@gmail.com>
2021-04-24 14:35:07 +1000

    Adding bar chart to show the number of event bookings a user has

---
+++ frontend/src/components/Bookings/BookingChart/BookingChart.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2'
+
+const BOOKINGS_BUCKETS = {
+    Cheap: { min: 0, max: 100 },
+    Normal: { min: 100, max: 200 },
+    Expensive: { min: 200, max: 100000000 }
+}
+
+const bookingsChart = props => {
+
+    const chartData = { labels: [], datasets: [] }
+    for (const bucket in BOOKINGS_BUCKETS) {
+        const filteredBookingsCount = props.bookings.reduce((prev, current) => {
+            if (BOOKINGS_BUCKETS[bucket].min < parseFloat(current.event.price) && parseFloat(current.event.price) <= BOOKINGS_BUCKETS[bucket].max) {
+                console.log('comparison', current.event.price, BOOKINGS_BUCKETS[bucket].max)
+                return prev + 1;
+            } else {
+                return prev;
+            }
+        }, 0);
+        chartData.labels.push(bucket)
+        chartData.datasets.push(filteredBookingsCount)
+    }
+    const data = {
+        labels: chartData.labels,
+        datasets: [
+            {
+                label: '# of Event',
+                data: chartData.datasets,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',,
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    };
+
+    return <Bar data={data} options={options} />
+}
+
+export default bookingsChart;
--- frontend/src/pages/Booking.js
+++ frontend/src/pages/Booking.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import AuthContext from '../context/auth-context'
 import Spinner from '../components/Spinner/Spinner'
 import BookingList from '../components/Bookings/BookingList/BookingList'
+import BookingsChart from '../components/Bookings/BookingChart/BookingChart'
+import BookingControl from '../components/Bookings/BookingControl/BookingControl'
 
 class BookingPage extends Component {
     state = {
         isLoading: false,
-        bookings: []
+        bookings: [],
+        outputType: 'list'
     }
 
     static contextType = AuthContext
@@ -26,6 +29,7 @@ class BookingPage extends Component {
                         _id
                         title
                         date
+                        price
                     }
                 }
             }
@@ -104,12 +108,34 @@ class BookingPage extends Component {
         this.fetchBookings()
     }
 
+    changeOutputTypeHandler = (outputType) => {
+        if (outputType === 'list') {
+            this.setState({ outputType: 'list' })
+        } else {
+            this.setState({ outputType: 'chart' })
+        }
+    }
+
     render() {
+        let content = <Spinner />;
+        if (!this.state.isLoading) {
+            content = (
+                <React.Fragment>
+                    <BookingControl activeOutputType={this.state.outputType}
+                        onChange={this.changeOutputTypeHandler} />
+                    <div>
+                        {this.state.outputType === 'list' ?
+                            <BookingList bookings={this.state.bookings} onDelete={this.deleteBookingHandler} /> :
+                            <BookingsChart bookings={this.state.bookings} />}
+                    </div>
+                </React.Fragment>
+            )
+        }
         return (
             <React.Fragment>
                 {this.state.isLoading ? <Spinner /> :
                     <ul>
-                        <BookingList bookings={this.state.bookings} onDelete={this.deleteBookingHandler} />
+                        {content}
                     </ul>
                 }
             </React.Fragment>
